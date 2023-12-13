5f0010c54fda2b57884bbc449b1605b06579b127
aleksandrnedakin <nedyasha@gmail.com>
2023-12-13 20:54:32 +0300

    2

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import NavigationButton, {Paths} from "./NavigationButton";
 import "./header.css"
+import icon from "../images/icon.png"
 
 const Header = () => {
     return (
         <div className="header">
             <div className="background-line">
                 <div className="container">
-                    <NavigationButton info={{ path: Paths.Home, label: "Home" }} />
-                    <NavigationButton info={{ path: Paths.About, label: "About" }} />
-                    <NavigationButton info={{ path: Paths.Developers, label: "Developers" }} />
-                    <NavigationButton info={{ path: Paths.Family, label: "Family" }} />
-                    <NavigationButton info={{ path: Paths.Member_Info, label: "Member inf" }} />
+                    <NavigationButton info={{ path: Paths.Home, label: "Главная" }} />
+                    <NavigationButton info={{ path: Paths.About, label: "О сайте" }} />
+                    <NavigationButton info={{ path: Paths.Family, label: "Семья Ямато" }} />
+                    <NavigationButton info={{ path: Paths.Developers, label: "Разработчики" }} />
+                    {/*<NavigationButton info={{ path: Paths.Member_Info, label: "" }} />*/}
                 </div>
             </div>
+            <img className="header_icon" src={icon}/>
         </div>
     );
 };
--- src/components/Home/HomeComponents/WelcomePage.tsx
+++ src/components/Home/HomeComponents/WelcomePage.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ".//WelcomePage.css"
+import img from "./img.png"
 
 const WelcomePage = () => {
     return (
         <div className="introduce-container">
             <div className="introduce-text">
-                <h1>Семья Ямато</h1>
+                <div className="title"><h1>Семья Ямато</h1></div>
             </div>
-            <div className="introduce-pictures"></div>
+            <div className="introduce-pictures">
+                <img className="welcome_img" src={img} alt="ooops"/>
+                <h2>皇室</h2>
+            </div>
         </div>
     );
 };
