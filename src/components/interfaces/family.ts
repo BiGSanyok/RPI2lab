a173929a777a9fd32487762a8a16a982198fa03d
quantum32043 <quantum32043@gmail.com>
2023-12-20 05:02:18 +0300

    yukuy

--- src/components/interfaces/family.ts
+++ src/components/interfaces/family.ts
@@ -1,20 +1,20 @@
 export interface IFamilyMemberDates{
-    "dateTitle": string;
-    "dateInfo":  string;
-    "url": string;
+    dateTitle: string;
+    dateInfo:  string;
+    url: string;
 }
 
 
 
 export interface IFamilyMemberData {
-    "name": string;
-    "id": number;
-    "birthYear": string;
-    "deathYear": string;
-    "description": string;
-    "cardInfo": string;
-    "youtube": string;
-    "maps": string;
-    "FamilyMemberDates": IFamilyMemberDates[];
+    name: string;
+    id: number;
+    birthYear: string;
+    deathYear: string;
+    description: string;
+    cardInfo: string;
+    youtube: string;
+    maps: string;
+    FamilyMemberDates: IFamilyMemberDates[];
 }
 
